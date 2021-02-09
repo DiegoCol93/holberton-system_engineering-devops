beac6b6a04c395e7bf2ab58babb7088b35aabf2b
Diego Lopez <dfelo93@gmail.com>
2021-02-08 19:10:33 -0500

    Necromancy program.

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int infinite_while(void);
+/**
+ * main       - Creates zombie child processes.
+ *     _____   __  ______  ______  ______
+ *    /\  __-./\ \/\  ___\/\  ___\/\  __ \
+ *    \ \ \/\ \ \ \ \  __\\ \ \__ \ \ \/\ \
+ *     \ \____-\ \_\ \_____\ \_____\ \_____\
+ *      \/____/ \/_/\/_____/\/_____/\/_____/
+ *     __      ______  ______  ______  ______
+ *    /\ \    /\  __ \/\  == \/\  ___\/\___  \
+ *    \ \ \___\ \ \/\ \ \  _-/\ \  __\\/_/  /__
+ *     \ \_____\ \_____\ \_\   \ \_____\/\_____\
+ *      \/_____/\/_____/\/_/    \/_____/\/_____/
+ *                                    Feb - 2021
+ */
+void main(void)
+{
+	unsigned int pid = 0;
+	int i = 0;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(1);
+		}
+		else
+			sleep(1);
+	}
+	infinite_while();
+}
+/**
+ * infinite_while - Starts an infinite loop.
+ * Return: 0 never.
+ */
+int infinite_while(void)
+{
+	while (1)
+		sleep(1);
+	return (0);
+}
