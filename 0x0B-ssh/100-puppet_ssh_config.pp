f8df7632f620a0c11ae17406fd5d73bcafcbd4f6
DiegoCol93 <dfelo93@gmail.com>
2021-04-02 13:12:24 -0500

    ADDED | Manifest for automatically setting the same configs as in the file 2-ssh_config.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config'
+  line => 'IdentityFile ~/.ssh/holberton'
+}
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config'
+  line => 'PasswordAuthentication no'
+}
