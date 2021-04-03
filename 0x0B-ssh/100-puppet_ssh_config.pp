0c8eaaeeec9e6ed29a3b5e00732774866445d026
DiegoCol93 <dfelo93@gmail.com>
2021-04-03 18:14:18 -0500

    ADDED | ensure attribute.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
 # Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
 file_line { 'Turn off passwd auth':
+  ensure => 'present'
   path => '/etc/ssh/ssh_config'
   line => '    IdentityFile ~/.ssh/holberton'
 }
 file_line { 'Declare identity file':
-  
-  path => '/etc/ssh/ssh_config'
-  line => '    PasswordAuthentication no'
+  ensure => 'present'
+  path   => '/etc/ssh/ssh_config'
+  line   => '    PasswordAuthentication no'
 }
