9bedb7eb2d59322c4c6c308299c7dec8f98837d6
DiegoCol93 <dfelo93@gmail.com>
2021-04-03 21:13:53 -0500

    FIXED | 'absent' to 'present'.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
 file_line { 'Turn off passwd auth':
-  ensure => 'absent',
+  ensure => 'present',
   path   => '~/.ssh/config',
   line   => '    IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'Declare identity file':
-  ensure => 'absent',
+  ensure => 'present',
   path   => '~/.ssh/config',
   line   => '    PasswordAuthentication no'
 }
