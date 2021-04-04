30f2e3c6a4bef1edd104e1d3f62c7c5270d8172f
DiegoCol93 <dfelo93@gmail.com>
2021-04-04 01:31:24 -0500

    FIXED | Commented out file section.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Sets up the ssh_config file for using holberton key and
 # no password on outbound ssh connections.
 
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-  user   => 'root',
-  mode   => '0644',
-  owner  => 'root',
-  group  => 'root'
-}
+# file { '/etc/ssh/ssh_config':
+#   ensure => present,
+#   user   => 'root',
+#   mode   => '0644',
+#   owner  => 'root',
+#   group  => 'root'
+# }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
