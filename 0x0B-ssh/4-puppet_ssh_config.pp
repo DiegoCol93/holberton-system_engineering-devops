7619bfe9455ee3d85d837349fe5b4ac5dd28aa4c
DiegoCol93 <dfelo93@gmail.com>
2021-04-04 00:35:21 -0500

    REMOVED | Extra ',' characters.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -8,11 +8,11 @@ file { '/etc/ssh/ssh_config':
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => 'etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+  line   => '    IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Declare identity file':
   ensure => present,
   path   => 'etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => '    PasswordAuthentication no'
 }
