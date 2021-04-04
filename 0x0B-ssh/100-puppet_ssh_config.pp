c440c7e195f906c552e221b9fbedf7bb29c8fc49
DiegoCol93 <dfelo93@gmail.com>
2021-04-04 00:52:33 -0500

    ADDED | Chaged name of file again.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,18 @@
-# Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
+# Sets up the ssh_config file for using holberton key and
+# no password on outbound ssh connections.
+
+file { '/etc/ssh/ssh_config':
+  ensure => present
+}
+
 file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '~/.ssh/config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '~/.ssh/config',
+  ensure => present,
+  path   => 'etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
