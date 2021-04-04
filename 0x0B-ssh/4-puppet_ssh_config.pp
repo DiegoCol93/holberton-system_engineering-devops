4f7a6c88bd697e47c395587ed60db0aae0928eb1
DiegoCol93 <dfelo93@gmail.com>
2021-04-03 22:32:52 -0500

    FIXED | Name of the file due to checker issues.

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+  user   => 'root'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => 'etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  user   => 'root'
+}
