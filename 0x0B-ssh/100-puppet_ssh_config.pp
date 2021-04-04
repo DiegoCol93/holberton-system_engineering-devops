6ed8ef16020aec919affcf1adc4e0d922dcd4184
DiegoCol93 <dfelo93@gmail.com>
2021-04-04 10:27:17 -0500

    | ADDED | Ensure file is present with root.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,22 @@
 # Sets up the ssh_config file for using holberton key and
 # no password on outbound ssh connections.
 
-file { '/etc/ssh/ssh_config':
-  ensure => present
+file { 'Ensure File':
+  ensure => present,
+  path    => '/etc/ssh/ssh_config',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
 }
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/holberton'
 }
 
 file_line { 'Declare identity file':
   ensure => present,
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
