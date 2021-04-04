ce4d34a0915d4d742b0876fd140fc7d8ec3eed6e
DiegoCol93 <dfelo93@gmail.com>
2021-04-03 19:46:05 -0500

    ADDED | Missing ',' and check for IdentityFile ocurrences.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,14 @@
 # Sets up the ssh_config file for using holberton key and no password outbound ssh connections.
 file_line { 'Turn off passwd auth':
-  ensure => 'present'
-  path => '/etc/ssh/ssh_config'
-  line => '    IdentityFile ~/.ssh/holberton'
+  ensure   => 'present',
+  path     => '/etc/ssh/ssh_config',
+  line     => '    IdentityFile ~/.ssh/holberton',
+  match    => '[IdentityFile].+',
+  multiple => true
 }
+
 file_line { 'Declare identity file':
-  ensure => 'present'
-  path   => '/etc/ssh/ssh_config'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
