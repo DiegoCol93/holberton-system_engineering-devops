1c64b1177d883c30f44b1fd0a4633e752fc6939d
DiegoCol93 <dfelo93@gmail.com>
2021-05-17 12:56:31 -0500

    |- FIXED -| limit=10 instead of 9.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ from requests import get
 def top_ten(subreddit):
     """ Prints the hottest 10 posts on a given subbreddit. """
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'HolbertonSchool'}
 
     r = get(url, headers=headers, allow_redirects=False)
