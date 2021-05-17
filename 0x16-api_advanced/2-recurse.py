3332079a9bf120398db02a36ce41cf576e6e76e2
DiegoCol93 <dfelo93@gmail.com>
2021-05-17 13:51:19 -0500

    |- ADDED -| Return for when subreddit is not found.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,19 +9,25 @@ def recurse(subreddit, hot_list=[], page_after=None):
     if page_after is None:
         url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
         r = get(url, headers=headers, allow_redirects=False)
-        if r.json()['data']['after'] is not None:
+        if r.status_code == 200:
             for child in r.json()['data']['children']:
                 hot_list.append(child['data']['title'])
-            recurse(subreddit, hot_list, r.json()['data']['after'])
+            if r.json()['data']['after'] is not None:
+                recurse(subreddit, hot_list, r.json()['data']['after'])
+        else:
+            return None
     else:
         url = ('https://www.reddit.com/r/{}/hot.json?after={}'
                .format(subreddit,
                        page_after))
         r = get(url, headers=headers, allow_redirects=False)
-        for child in r.json()['data']['children']:
-            hot_list.append(child['data']['title'])
 
-        if r.json()['data']['after'] is not None:
-            recurse(subreddit, hot_list, r.json()['data']['after'])
+        if r.status_code == 200:
+            for child in r.json()['data']['children']:
+                hot_list.append(child['data']['title'])
+            if r.json()['data']['after'] is not None:
+                recurse(subreddit, hot_list, r.json()['data']['after'])
+        else:
+            return None
 
-    return (hot_list)
+    return hot_list
